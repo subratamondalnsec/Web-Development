9a2bdd29e0f2d1a1671a5a3a7a04f813676627d0
subratamondal2 <subrata24012005@gmail.com>
2025-07-15 08:30:04 +0530

    RateLimiter add use Fixed Algo

---
+++ 07 Backend/Rohit_Bhaiya/Day25_RateLimiter/middleware/rateLimiter.js
@@ -0,0 +1,39 @@
+const redisClient= require("../config/redis")
+
+
+const rateLimiter= async (req,res,next)=>{
+
+    try{
+        const ip = req.ip;
+        console.log(ip);
+
+        // Kya ye Ip exist karta hai
+
+        // nahi karta
+        // set method redisClient.set(ip,`1:${Date.now()/1000}`)
+        // await redisClient.expire(3600);
+
+
+        // Exist karta hga:
+        // get
+
+        const Number_of_request= await redisClient.incr(ip);
+        console.log(Number_of_request);
+
+        if(Number_of_request==1){
+            await redisClient.expire(3600);
+        }
+
+        if(Number_of_request>5){
+            throw new Error("Use Limit Exceeded")
+        }
+
+        next();
+    }
+    catch(err){
+        res.send("Error: "+err);
+    }
+
+}
+
+module.exports = rateLimiter;
